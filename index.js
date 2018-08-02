5a81bf62789ed6e40d9734e3712cefa21349a9cd
Sharmina Akhtar <sakht318@gmail.com>
2018-08-02 13:48:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Code your solution in this file.
 // Code your solution in this file.
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
-function lowerCaseDrivers(list) {
-  return list.map(function (driver) {
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (driver) {
   return driver.toLowercase();
-});
+  })
 }
+console.log(lowerCaseDrivers(drivers));
 
 function nameToAttributes(list) {
   return list.map( function (driver) {
