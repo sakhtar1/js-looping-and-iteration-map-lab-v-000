96cf82d0f89f59a3782e63df3e4beeecbdc15888
Sharmina Akhtar <sakht318@gmail.com>
2018-08-02 13:44:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // Code your solution in this file.
 // Code your solution in this file.
-function lowerCaseDrivers (list) {
+function lowerCaseDrivers(list) {
   return list.map(function (driver) {
-    return driver.toLowerCase();
-  });
+  return driver.toLowercase();
+});
 }
-
 
 function nameToAttributes(list) {
   return list.map( function (driver) {
     const firstName = driver.split(' ')[0];
     const lastName = driver.split(' ')[1];
-    return {firstName, lastName
-  })
+    return {firstName, lastName;
+  });
 }
